05b4a78ebe70dc29d5eaca8cee98f66f69e33bf5
Naman Agrawal <122742832+GitnamanHub@users.noreply.github.com>
2024-02-04 08:35:33 +0530

    Update snakegame.cpp

--- snakegame.cpp
+++ snakegame.cpp
@@ -1,63 +1,91 @@
-#include<iostream>
-#include<ctime>
-#include<conio.h>
-#include<Windows.h>
+#include <iostream>
+#include <ctime>
+#include <conio.h>
+#include <Windows.h>
 using namespace std;
 
 bool gameover;
-int height=20,width=20;
-int x,y;
-int fruitX,fruitY;
+int height = 20, width = 20;
+int x, y;
+int fruitX, fruitY;
 int score;
+int tailX[100], tailY[100];
+int Ltail{0};
 
-enum direction{Left=1, Right, Up, Down};
+enum direction
+{
+    Left = 1,
+    Right,
+    Up,
+    Down
+};
 direction dir;
 
 void draw()
 {
-    system("cls");
-    for(int i=0;i<height;i++)
+    // To stop flickering of screen
+    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), {0, 0}); // windows.h
+
+    // To stop flickering of cursor
+    CONSOLE_CURSOR_INFO info;
+    info.dwSize = 100;
+    info.bVisible = false;
+    SetConsoleCursorInfo(GetStdHandle(STD_OUTPUT_HANDLE), &info); // windows.h
+
+    for (int i = 0; i < height; i++)
     {
-        for(int j=0;j<width;j++)
+        for (int j = 0; j < width; j++)
         {
-            if(i==0 || i == height-1)
-                cout<<"# ";
+            if (i == 0 || i == height - 1)
+                cout << "# ";
 
-            else if(j==0 || j==width-1)
-                cout<<"# ";
+            else if (j == 0 || j == width - 1)
+                cout << "# ";
 
-            else if(i==fruitY && j==fruitX)
-                cout<<"F ";
+            else if (i == fruitY && j == fruitX)
+                cout << "F ";
 
-            else if(i==y && j==x)
-                cout<<"O ";
+            else if (i == y && j == x)
+                cout << "O ";
 
             else
-                cout<<"  ";
-
+            {
+                bool print = false;
+                for(int k = 0; k < Ltail; k++)
+                {
+                    if(tailX[k]==j && tailY[k]==i)
+                    {
+                        cout << "o ";
+                        print = true;
+                    }
+                }
+                if(!print)
+                cout << "  ";
+            }
+                
         }
 
-        cout<<endl;
+        cout << endl;
     }
 
-    cout<<"Score: "<<score<<endl;
+    cout << "Score: " << score << endl;
 }
 
 void setup()
 {
     gameover = false;
-    x=width/2;
-    y=height/2;
-    fruitX=rand()%(width-2) +1;
-    fruitY=rand()%(height-2) +1;
-    score=0;
+    x = width / 2;
+    y = height / 2;
+    fruitX = rand() % (width - 2) + 1;
+    fruitY = rand() % (height - 2) + 1;
+    score = 0;
 }
 
 void input()
 {
-    if(_kbhit())
+    if (_kbhit()) // input from keyboard
     {
-        switch (_getch())
+        switch (_getch()) // conio.h lib
         {
         case 'a':
             dir = Left;
@@ -75,13 +103,27 @@ void input()
             gameover = true;
             break;
         }
-
     }
 }
 
 void logic()
 {
-    switch(dir)
+    int prevX, prevY;
+    int prev2X, prev2Y;
+    prevX = tailX[0];
+    prevY = tailY[0];
+    tailX[0] = x;
+    tailY[0] = y;
+    for (int i = 1; i < Ltail; i++)
+    {
+        prev2X = tailX[i];
+        prev2Y = tailY[i];
+        tailX[i] = prevX;
+        tailY[i] = prevY;
+        prevX = prev2X;
+        prevY = prev2Y;
+    }
+    switch (dir)
     {
     case Left:
         x--;
@@ -99,27 +141,31 @@ void logic()
         break;
     }
 
-    //checking collision
-    if(x<=0 || x>=width-1 || y<=0 || y>=height-1)
+    // checking collision
+    if (x <= 0 || x >= width - 1 || y <= 0 || y >= height - 1)
     {
         gameover = true;
     }
 
-    if(x==fruitX && y==fruitY)
+    for(int i = 0; i < Ltail; i++)
     {
-        score+=10;
-        fruitX=rand()%(width-2)+1;
-        fruitY=rand()%(height-2)+1;
+        if(tailX[i] == x && tailY[i] == y)
+        gameover = true;
     }
 
-
+    if (x == fruitX && y == fruitY)
+    {
+        score += 10;
+        fruitX = rand() % (width - 2) + 1;
+        fruitY = rand() % (height - 2) + 1;
+        Ltail++;
+    }
 }
-
 
 int main()
 {
     setup();
-    while(!gameover)
+    while (!gameover)
     {
         srand(time(0));
         draw();
@@ -128,6 +174,6 @@ int main()
         Sleep(100);
     }
 
-    cout<<"Game over!"<<endl;
-    cout<<"Your Score: "<<score<<endl;
+    cout << "Game over!" << endl;
+    cout << "Your Score: " << score << endl;
 }
